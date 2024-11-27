b4510bef8c02642d8fb88328dccb267f55ff32ee
zbagheri6 <z.bagheri987@gmail.com>
2024-11-27 12:17:19 +0330

    Add files via upload

---
+++ CircleArea.cs
@@ -0,0 +1,15 @@
+namespace Circle
+{
+    public class CircleArae()
+    {
+        public void CalculateArae()
+        {
+            Console.Write("Enter Number: ");
+            var radius = int.Parse(Console.ReadLine()):
+            var area =3.14 *(radius * radius);
+            Console. WriteLine(area);
+        }
+    }
+}
+
+      
